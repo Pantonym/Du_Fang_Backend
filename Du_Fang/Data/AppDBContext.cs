91e0b921261c4be9a0d8592aecb6d54a1ca4c52d
Pantonym <221179@virtualwindow.co.za>
2024-08-05 14:48:22 +0200

    setup

---
+++ Du_Fang/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Du_Fang;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+    // TODO: Specify endpoint controllers
+    // public DbSet<ToDoItem> ToDoItems { get; set; }
+
+}
