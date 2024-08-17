9685a7a4f86eaf8b0380ba7258795b83afcd174a
Pantonym <221179@virtualwindow.co.za>
2024-08-17 10:47:06 +0200

    relaunch changes

--- Du_Fang/obj/Debug/net8.0/Du_Fang.AssemblyInfo.cs
+++ Du_Fang/obj/Debug/net8.0/Du_Fang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Du_Fang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898df0dd312893d86d7ade75cd020b3da924f48f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbeb6550e3abe1fc3596b36a4153a60c4d17062b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Du_Fang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Du_Fang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
